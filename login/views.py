4720fe521cc170bf5ea79d1ada1cdbd335eaa0d9
leo <geek_leo@outlook.com>
2018-08-15 15:32:48 +0800

    complete the basic site

--- login/views.py
+++ login/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
+from . import models
+from . import forms
+import hashlib
 
 
 # Create your views here.
+
+def hash_code(s, salt='userSystem'):
+    h = hashlib.sha256()
+    s += salt
+    h.update(s.encode())
+    return h.hexdigest()
 
 
 def index(request):
@@ -11,15 +20,68 @@ def index(request):
 
 
 def login(request):
-    pass
-    return render(request, 'login/login.html')
+    if request.session.get('is_login', None):
+        return redirect("/index/")
+    if request.method == "POST":
+        login_form = forms.UserForm(request.POST)
+        message = 'All field are required'
+        if login_form.is_valid():
+            username = login_form.cleaned_data['username']
+            password = login_form.cleaned_data['password']
+            try:
+                user = models.User.objects.get(name=username)
+                if user.password == hash_code(password):
+                    request.session['is_login'] = True
+                    request.session['user_id'] = user.id
+                    request.session['user_name'] = user.name
+                    return redirect('/index/')
+                else:
+                    message = 'wrong password'
+            except:
+                message = 'wrong username'
+        return render(request, 'login/login.html', locals())
+    login_form = forms.UserForm()
+    return render(request, 'login/login.html', locals())
 
 
 def register(request):
-    pass
-    return render(request, 'login/register.html')
+    if request.session.get('is_login', None):
+        return redirect('/index/')
+    if request.method == "POST":
+        register_form = forms.RegisterForm(request.POST)
+        message = "Please check the field"
+        if register_form.is_valid():
+            username = register_form.cleaned_data['username']
+            password1 = register_form.cleaned_data['password1']
+            password2 = register_form.cleaned_data['password2']
+            email = register_form.cleaned_data['email']
+            sex = register_form.cleaned_data['sex']
+            if password1 != password2:
+                message = "两次输入密码不相同！"
+                return render(request, 'login/register.html', locals())
+            else:
+                same_name_user = models.User.objects.filter(name=username)
+                if same_name_user:
+                    message = "用户名已存在，请重新选择用户名！"
+                    return render(request, 'login/register.html', locals())
+                same_email_user = models.User.objects.filter(email=email)
+                if same_email_user:
+                    message = "该邮箱地址已被注册，请更换其他邮箱！"
+                    return render(request, 'login/register.html', locals())
+
+                new_user = models.User()
+                new_user.name = username
+                new_user.password = hash_code(password1)
+                new_user.email = email
+                new_user.sex = sex
+                new_user.save()
+                return redirect('/login/')
+    register_form = forms.RegisterForm()
+    return render(request, 'login/register.html', locals())
 
 
 def logout(request):
-    pass
+    if not request.session.get('is_login', None):
+        return redirect("/index/")
+    request.session.flush()
     return redirect("/index/")
--- userSystem/settings.py
+++ userSystem/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'login',
+    'captcha',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -126,3 +127,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.sina.com'
+EMAIL_PORT = 25
+EMAIL_HOST_USER = 'geek_leo@sina.com'
+EMAIL_HOST_PASSWORD = 'wj927102lyc'
